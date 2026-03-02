41ef1aebd3e05c0de8acd7757c57a7b4be18fb74
saundersresearch <adam.m.saunders@vanderbilt.edu>
2026-03-02 02:00:03 -0600

    Update radio log 2026-03-02 02:00:03

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source "https://rubygems.org"
+
+gem "github-pages", group: :jekyll_plugins
+gem "jekyll-include-cache", group: :jekyll_plugins
+gem "jekyll-webmention_io", "4.1.0", group: :jekyll_plugins
+gem "jekyll-remote-theme", group: :jekyll_plugins
+
+gem "jekyll-redirect-from", "~> 0.16.0"
+gem "multi_json", ">= 1.15", "<= 1.18.0"
+
+gem "minimal-mistakes-jekyll", "~> 4.27.3"
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,163 @@
+var store = [{
+        "title": "1769302932",
+        "excerpt":"Testing a post with Apple Shortcuts  ","categories": [],
+        "tags": ["IndieWeb"],
+        "url": "/notes/2026/01/24/1769302932/",
+        "teaser": null
+      },{
+        "title": "1769306323",
+        "excerpt":"Testing a post with Apple Shortcuts  ","categories": [],
+        "tags": ["IndieWeb"],
+        "url": "/notes/2026/01/24/1769306323/",
+        "teaser": null
+      },{
+        "title": "1769704401",
+        "excerpt":"Testing a post with single-item lists  ","categories": [],
+        "tags": ["IndieWeb"],
+        "url": "/notes/2026/01/29/1769704401/",
+        "teaser": null
+      },{
+        "title": "1769705577",
+        "excerpt":"Testing if I can update  ","categories": [],
+        "tags": ["test"],
+        "url": "/notes/2026/01/29/1769705577/",
+        "teaser": null
+      },{
+        "title": "1769706023",
+        "excerpt":"Testing if I can update  ","categories": [],
+        "tags": ["test"],
+        "url": "/notes/2026/01/29/1769706023/",
+        "teaser": null
+      },{
+        "title": "1769706073",
+        "excerpt":"Testing if I can update  ","categories": [],
+        "tags": ["test"],
+        "url": "/notes/2026/01/29/1769706073/",
+        "teaser": null
+      },{
+        "title": "1769706169",
+        "excerpt":"Testing if I can update  ","categories": [],
+        "tags": ["test"],
+        "url": "/notes/2026/01/29/1769706169/",
+        "teaser": null
+      },{
+        "title": "1769706231",
+        "excerpt":"Testing if I can update  ","categories": [],
+        "tags": ["test"],
+        "url": "/notes/2026/01/29/1769706231/",
+        "teaser": null
+      },{
+        "title": "1769706259",
+        "excerpt":"Testing if I can update  ","categories": [],
+        "tags": ["test"],
+        "url": "/notes/2026/01/29/1769706259/",
+        "teaser": null
+      },{
+        "title": "1769706291",
+        "excerpt":"Testing if I can add just single syndicate-to  ","categories": [],
+        "tags": ["IndieWeb"],
+        "url": "/notes/2026/01/29/1769706291/",
+        "teaser": null
+      },{
+        "title": "1769706594",
+        "excerpt":"Testing if I can update  ","categories": [],
+        "tags": ["test"],
+        "url": "/notes/2026/01/29/1769706594/",
+        "teaser": null
+      },{
+        "title": "1769706896",
+        "excerpt":"Testing if I can update  ","categories": [],
+        "tags": ["test"],
+        "url": "/notes/2026/01/29/1769706896/",
+        "teaser": null
+      },{
+        "title": "1769706959",
+        "excerpt":"Testing if I can add just single syndicate-to  ","categories": [],
+        "tags": ["IndieWeb"],
+        "url": "/notes/2026/01/29/1769706959/",
+        "teaser": null
+      },{
+        "title": "1769740381",
+        "excerpt":"Testing if I can update  ","categories": [],
+        "tags": ["test"],
+        "url": "/notes/2026/01/29/1769740381/",
+        "teaser": null
+      },{
+        "title": "1769740549",
+        "excerpt":"Testing if I can add just single syndicate-to  ","categories": [],
+        "tags": ["IndieWeb","tag2"],
+        "url": "/notes/2026/01/29/1769740549/",
+        "teaser": null
+      },{
+        "title": "1772163356",
+        "excerpt":"hello world  ","categories": [],
+        "tags": ["foo","bar"],
+        "url": "/notes/2026/03/01/1772163356/",
+        "teaser": null
+      },{
+        "title": "1772163762",
+        "excerpt":"hello world  ","categories": [],
+        "tags": ["foo","bar"],
+        "url": "/notes/2026/03/01/1772163762/",
+        "teaser": null
+      },{
+        "title": "1772163775",
+        "excerpt":"hello world  ","categories": [],
+        "tags": ["foo","bar"],
+        "url": "/notes/2026/03/01/1772163775/",
+        "teaser": null
+      },{
+        "title": "1772163819",
+        "excerpt":"hello world  ","categories": [],
+        "tags": ["foo","bar"],
+        "url": "/notes/2026/03/01/1772163819/",
+        "teaser": null
+      },{
+        "title": "1772163854",
+        "excerpt":"hello world  ","categories": [],
+        "tags": ["foo","bar"],
+        "url": "/notes/2026/03/01/1772163854/",
+        "teaser": null
+      },{
+        "title": "1772163858",
+        "excerpt":"hello world  ","categories": [],
+        "tags": ["foo","bar"],
+        "url": "/notes/2026/03/01/1772163858/",
+        "teaser": null
+      },{
+        "title": "1772163975",
+        "excerpt":"hello world  ","categories": [],
+        "tags": ["foo","bar"],
+        "url": "/notes/2026/03/01/1772163975/",
+        "teaser": null
+      },{
+        "title": "1772205561",
+        "excerpt":"hello world  ","categories": [],
+        "tags": ["foo","bar"],
+        "url": "/notes/2026/03/01/1772205561/",
+        "teaser": null
+      },{
+        "title": "1772206125",
+        "excerpt":"hello world  ","categories": [],
+        "tags": ["foo","bar"],
+        "url": "/notes/2026/03/01/1772206125/",
+        "teaser": null
+      },{
+        "title": "1772246977",
+        "excerpt":"hello world  ","categories": [],
+        "tags": ["foo","bar"],
+        "url": "/notes/2026/03/01/1772246977/",
+        "teaser": null
+      },{
+        "title": "My Amazing Post!",
+        "excerpt":"hello world  ","categories": [],
+        "tags": ["foo","bar"],
+        "url": "/posts/2026/02/26/my-amazing-post/",
+        "teaser": null
+      },{
+        "title": "My Amazing Post!",
+        "excerpt":"hello world  ","categories": [],
+        "tags": ["foo","bar"],
+        "url": "/posts/2026/02/27/my-amazing-post/",
+        "teaser": null
+      },]
